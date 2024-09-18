f00e864172dbce57ff354e3f5b2dcf0d7043a1af
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2024-09-18 13:06:21 +0400

    add last two sections of page  and there their functionalities

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const rewardsSection = document.querySelector(`.reward-section`);
 const rewardOptions = document.querySelector(`.reward-options`);
 const enterPledgeArr = document.querySelectorAll(`.Enter-your-pledge`);
 const inStockInputsArr = document.querySelectorAll(`.inStock-inp`);
+const sectionThanksFor = document.querySelector(`.section--thanks-for`);
+const gotIt = document.querySelector(`.got-it--btn`);
 
 const menuBtn = document.querySelectorAll(`.menu-btn`);
 const darckBackround = document.querySelector(`.opened-menu--background`);
@@ -67,6 +69,40 @@ rewardOptions.addEventListener(`click`, (e) => {
             enterPledge.classList.remove(`display-none`)
         }
 
+        const continueBtn = enterPledge.querySelector(`.continue-btn`);
+        let pledgeAmount = '';
+        let minPledge = '';
+
+        if(Array.from(enterPledge.children).some(el => el.classList.contains(`continue-div`))){
+            pledgeAmount = enterPledge.querySelector(`.pledge-amount`);
+            minPledge = articlePerent.querySelector(`.min-pledge`);
+        }
+
+        continueBtn.addEventListener(`click`, () => {
+
+            if(pledgeAmount !== ``) {
+
+                if(pledgeAmount.value >= Number(minPledge.textContent)) {
+                    sectionThanksFor.classList.remove(`display-none`);
+                    rewardsSection.classList.add(`display-none`)
+                    enterPledgeArr.forEach(el => el.classList.add(`display-none`));
+                    inStockInputsArr.forEach(el => el.checked = false)
+                } else {
+                    console.log(`by`)
+                }
+            } else {
+                sectionThanksFor.classList.remove(`display-none`);
+                rewardsSection.classList.add(`display-none`)
+                enterPledgeArr.forEach(el => el.classList.add(`display-none`));
+                inStockInputsArr.forEach(el => el.checked = false)
+            }
+        })
+
     }
+
+
 })
 
+gotIt.addEventListener(`click`, () => {
+    sectionThanksFor.classList.add(`display-none`);
+})
